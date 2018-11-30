341dc6c4aa04bd7dd040a4502a3da97c7ff866c8
AlexXDD <OlehI.Demydenko@gmail.com>
2018-11-30 03:36:55 +0200

    What's new
    - Added 2 playable tracks
    - Added sountrack for each scene
    - Improved GUI. Added level choosing and loading
    - Implemented car selection feature
    - Implemented scenario from launch to level
    - A lot of minor changes and imrovments

---
+++ Assets/_Scripts/Scene/CarSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour {
+    [Header("Vehicles")]
+    public List<GameObject> vehicles;
+
+    [Header("Spawn Loaction")]
+    public Transform spawnLocation;
+
+    [Header("Chase cam")]
+    public RVP.CameraControl camera;
+
+    private GameObject vehicle;
+	// Use this for initialization
+	void Start () {
+        vehicle = Instantiate(vehicles[CurrentVehicle.currentVehicleId], spawnLocation.transform.position, Quaternion.Euler(0, -168, 0)) as GameObject;
+        vehicle.SetActive(true);
+
+        camera.target = vehicle.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/Static/CurrentVehicle.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentVehicle {
+    //stores chosen car to load in level
+    public static int currentVehicleId { get; set; }
+}
